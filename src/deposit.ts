146d80cf633bce0416338dab0984efcaf643a7d7
Jupiterxiaoxiaoyu <s2068649@ed.ac.uk>
2025-03-09 12:35:26 +0800

    retry

--- src/deposit.ts
+++ src/deposit.ts
@@ -136,7 +136,7 @@ export class Deposit {
           address,
           pid_1,
           pid_2,
-          amount: amount / 10 ** 16,
+          amount: amount / BigInt(10 ** 16),
         });
         await tx.save();
         console.log(`Transaction hash and details saved: ${event.transactionHash}`);
